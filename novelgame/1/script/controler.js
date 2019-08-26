30403d4c4e9a970682784a3a07eeded11972126a
Aoi Matsumoto <AO2324@Aois-MacBook-Air.local>
2019-08-27 00:24:52 +0900

    Initial commit

--- novelgame/1/script/controler.js
+++ novelgame/1/script/controler.js
@@ -3,6 +3,13 @@ let storage = localStorage;
 let setting_data;
 
 let save_data;
+let save_screen_data;
+
+let loading_num;
+
+if (storage.getItem('save_data') == '{}'){
+    storage.setItem('save_data', JSON.stringify([]) );
+}
 
 if (storage.length == 0){
     console.log("null")
@@ -10,12 +17,13 @@ if (storage.length == 0){
     setting_data = [0,0,0.5,0.5]; // 文字の大きさ　文字スピード　オーディオ設定
     storage.setItem('setting_data', JSON.stringify(setting_data) );
     save_data = []; // save_data_name, scene_num, scene_count, bgm_num, bgi_num, char1, char2, still
-    storage.setItem('save_data', JSON.stringify([]) );
+    storage.setItem('save_data', JSON.stringify(save_data) );
 } else {
     setting_data = JSON.parse(storage.getItem('setting_data') );
     save_data = JSON.parse(storage.getItem('save_data') );
 }
-console.log(storage);
+console.log(setting_data);
+console.log(save_data);
 
 const scene_default = document.getElementById("scene").outerHTML;
 console.log(scene_default);
@@ -94,6 +102,28 @@ function font_size_btn(num){
     storage.setItem('setting_data', JSON.stringify(setting_data) );
 }
 
+function data_load(data_num){
+    let loaded_data = save_data[data_num];
+    frame_name[frame_number].style.display = 'none';
+    frame_name[frame_number = 2].style.display = 'block';
+    console.log(frame_name[frame_number]);
+    scene_number = loaded_data[1];
+    scene_count = loaded_data[2];
+    document.getElementById("scene_box").innerHTML = loaded_data[3];
+    document.getElementById("choice_window").innerHTML = "";
+    frame_name[2] = document.getElementById("scene");
+    scene_play();
+    console.log( loaded_data[4]);
+    if( loaded_data[4] != -1){
+        if ( bgm_num != -1) {
+            bgm_list[bgm_num].pause();
+        }
+        bgm_num = loaded_data[4];
+        bgm_list[ loaded_data[4]].play();
+    }
+    
+}
+
 function btn(key){
     console.log(key);
     switch (key) {
@@ -111,7 +141,9 @@ function btn(key){
                 frame_name[frame_number = 0].style.display = 'block';
                 scene_number = -1;
                 scene_count = 0;
-                bgm_list[bgm_num].pause();
+                if ( bgm_num != -1) {
+                    bgm_list[bgm_num].pause();
+                }
                 bgm_list[bgm_num].currentTime = 0;
                 bgm_num = -1;
                 document.getElementById("scene_box").innerHTML = scene_default;
@@ -135,13 +167,29 @@ function btn(key){
             break;
         case "saves":
                 frame_name[5].style.display = 'block';
+                for (let count = 0; count < save_data.length; count++){
+                    document.getElementById("data_box").insertAdjacentHTML('beforeend', '<button onclick="loading_num = '+ count +'">'+
+                    save_data[count][0][0] +'/'+ save_data[count][0][1] +'/'+ save_data[count][0][2] +'　シーン' + save_data[count][1] + '-' + save_data[count][2] + '</button>');
+                }
                 console.log(frame_name[5]);
             break;
+        case "load":
+                if(loading_num != null) data_load(loading_num);
         case "close_savesWindow":
                 frame_name[5].style.display = 'none';
+                document.getElementById("data_box").innerHTML = '';
             break;
         case "save":
+                document.getElementById("save_data_form").innerHTML = 'シーン' + scene_number + '-' + scene_count + '"を<br>保存しますか？';
                 frame_name[6].style.display = 'block';
+            break;
+        case "saved":
+            let date = new Date();
+                save_screen_data = document.getElementById("scene").outerHTML;
+                save_data.unshift([ [date.getFullYear(), date.getMonth()+1, date.getDate() ], scene_number, scene_count, save_screen_data, bgm_num]);
+                console.log(save_data);
+                storage.setItem('save_data', JSON.stringify(save_data));
+                frame_name[6].style.display = 'none';
             break;
 
         case "close_saveWindow":
